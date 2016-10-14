eea490df143bfbb5775de31cb328e38503b08e10
Felix Schueler <felix.schueler@ibm.com>
2016-10-14 15:41:29 -0700

    Add NMF algorithm

--- emma-sysml-dsl/src/test/scala/eu/stratosphere/emma/sysml/macros/RewriteMacrosSpec.scala
+++ emma-sysml-dsl/src/test/scala/eu/stratosphere/emma/sysml/macros/RewriteMacrosSpec.scala
@@ -34,6 +34,29 @@ import scala.util.Random
 @RunWith(classOf[JUnitRunner])
 class RewriteMacrosSpec extends FreeSpec with Matchers {
 
+  "NMF" in {
+
+    val nmf = parallelize {
+      val tfidf = Array(1.0, 2.0, 3.0, 4.0) // tfidf feature matrix coming from somewhere
+      val k = 40
+      val m, n = 2 // dimensions of tfidf
+      val maxIters = 200
+
+      val V = Matrix(tfidf, m, n) // initialize matrices
+      var W = Matrix.rand(m, k)
+      var H = Matrix.rand(k, n)
+
+      for (i <- 0 to maxIters) { //main loop
+        H = H * (W.t %*% V) / (W.t %*% (W %*% H))
+        W = W * (V %*% H.t) / (W %*% (H %*% H.t))
+      }
+
+      (W, H) // return values
+    }
+
+    val (w, h) = nmf.run()
+  }
+
 
   "Matrix Multiplication" in {
 
